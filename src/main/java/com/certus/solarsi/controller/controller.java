0db60e1a5284c89ab5909c8e11888e89d928dd77
Victor75824660 <75824660@certus.edu.pe>
2022-10-25 22:11:37 -0500

    aea

---
+++ src/main/java/com/certus/solarsi/controller/controller.java
@@ -0,0 +1,51 @@
+package com.certus.solarsi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/App")
+public class controller {
+	
+	@Value("${title.generic}") //Llamamos al valor de Properties y lo seteamos en la clase
+	private String titlePage;
+	
+	@GetMapping({"/inicio","/"})
+	public String Entrada(Model model) {
+	
+		
+	model.addAttribute("TituloPagina", titlePage); //Inyectar datos a la vista a través del controlador 
+
+		
+	return "Inicio";
+	
+	}
+	
+	@GetMapping("/Integrantes")
+	public String Integrante() {
+		
+		return "Integrantes";
+	}
+	
+	@GetMapping("/Login")
+	public String Log() {
+	
+		return "Login";
+	}
+	
+	@GetMapping("/Productos")
+	public String Productos() {
+	
+		return "Productos";
+	}
+	
+	@GetMapping("/CarritodeCompras")
+	public String CarritodeCompras() {
+	
+		return "CarritodeCompras";
+	}
+	
+}
